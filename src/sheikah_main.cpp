343c6e00ed2b5b24b48bf3f1313de6fe1a9a6d74
Lena Voytek <lena@voytek.dev>
2025-05-18 20:20:45 -0400

    Add default setup

---
+++ src/sheikah_main.cpp
@@ -0,0 +1,12 @@
+#include <miral/minimal_window_manager.h>
+#include <miral/runner.h>
+#include <miral/set_window_management_policy.h>
+
+using namespace miral;
+
+int main(int argc, char const *argv[]) {
+  MirRunner runner{argc, argv};
+
+  return runner.run_with(
+      {set_window_management_policy<MinimalWindowManager>()});
+}
