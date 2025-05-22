825d5228398ee1deea7d2e78e0e0815a2c5d12a9
Lena Voytek <lena@voytek.dev>
2025-05-22 17:02:04 -0400

    Update main formatting

--- src/sheikah_main.cpp
+++ src/sheikah_main.cpp
@@ -1,3 +1,21 @@
+/*
+ * sheikahWM
+ * Copyright (C) 2025  Lena Voytek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <miral/minimal_window_manager.h>
 #include <miral/runner.h>
 #include <miral/set_window_management_policy.h>
@@ -7,6 +25,5 @@ using namespace miral;
 int main(int argc, char const *argv[]) {
   MirRunner runner{argc, argv};
 
-  return runner.run_with(
-      {set_window_management_policy<MinimalWindowManager>()});
+  return runner.run_with({set_window_management_policy<MinimalWindowManager>()});
 }
